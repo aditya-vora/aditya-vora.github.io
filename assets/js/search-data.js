0d65a0230899007791c6b195a450fb493067d64d
aditya-vora <voraaditya898@gmail.com>
2025-12-03 05:20:03 +0000

    Deploying to gh-pages from @ aditya-vora/aditya-vora.github.io@f58b51e0d2b298c1ce98c19c8501b0c0e0533f7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,6 +481,13 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/aditya-vora-b66b1a58", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
