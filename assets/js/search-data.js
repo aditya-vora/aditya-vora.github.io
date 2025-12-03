cc650b31b1111ceff2691db3ceb670a0bbb76720
aditya-vora <voraaditya898@gmail.com>
2025-12-03 05:36:45 +0000

    Deploying to gh-pages from @ aditya-vora/aditya-vora.github.io@b7d38eddb05f7ea950323a3a9522bc5c6d10b13c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,18 +460,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%6F%72%61%61%64%69%74%79%61%38%39%38@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-linkedin',
